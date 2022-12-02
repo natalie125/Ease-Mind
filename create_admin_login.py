00e5883217587d4e1639ae70a004eb652d71b9d1
alex.cheong <alex.cheong@somin.ai>
2022-12-02 13:43:55 +0000

    added tests for login database to unit tests, modified create_admin_login.py to add email instead of username

--- create_admin_login.py
+++ create_admin_login.py
@@ -1,6 +1,6 @@
 from app import db, models
 
 # this file exists to run a script that will add our admin login
-admin = models.User_Login(username="admin", password="admin")
+admin = models.User_Login(email="admin@gmail.com", password="admin")
 db.session.add(admin)
 db.session.commit()
