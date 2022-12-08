f29ad3ffbdd4f04227af3a0b4c5795a8d8e7265c
ramat✨ <wonuola01@yahoo.co.uk>
2022-12-08 12:10:05 +0000

    add validation tests

--- cypress/functional_tests/login.cy.js
+++ cypress/functional_tests/login.cy.js
@@ -13,4 +13,21 @@ describe("Log In", () => {
 			cy.url().should("eq", "http://localhost:3000/signup");
 		});
 	});
+
+	describe("Data Validation", () => {
+		describe("When the user enters no input and tries to log in", () => {
+			it("Should stay on the log in page", () => {
+				cy.get(elements.Login_Button).click();
+				cy.url().should("eq", "http://localhost:3000/");
+			});
+			it("Should give an error message", () => {
+				cy.get(elements.Login_Button).click();
+				cy.get(elements.Login_Error_Text).should("be.visible"); // Assert that error text is visible
+				cy.get(elements.Login_Error_Text).should(
+					"have.text",
+					"Please enter a username and password"
+				); //Assert that the correct text is displayed
+			});
+		});
+	});
 });
