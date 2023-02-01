12af25303ac7e530df9378ed1009ec5ce54c9815
ramat✨ <wonuola01@yahoo.co.uk>
2023-02-01 16:09:27 +0000

    refactor register

--- src/Component/SignUp/SignUp.js
+++ src/Component/SignUp/SignUp.js
@@ -2,39 +2,32 @@ import axios from "axios";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-
-axios.interceptors.response.use(undefined, err => {
+axios.interceptors.response.use(undefined, (err) => {
 	const error = err.response;
 	return error;
 });
 
+const handleSubmit = async (email, password) => {
+	var data = {
+		email: email,
+		password: password,
+	};
 
-
-const handleSubmit = async (regAttempt) => {
-	const response = await axios.post("http://127.0.0.1:5000/register",
-	JSON.stringify({regAttempt}),
-	{
+	const response = await axios.post("http://127.0.0.1:5000/register", data, {
 		headers: {
 			"Content-Type": "application/json",
-		  'Access-Control-Allow-Origin' : true,
+			"Access-Control-Allow-Origin": true,
 		},
-	}
-	).then ((response) => {
-		return response;
 	});
+
 	return response;
-}
-
-
-
-
-
+};
 
 const SignUp = ({ email, password }) => {
 	const [isValid, setIsValid] = React.useState(null);
 	const navigate = useNavigate();
 
-	const validatesignup = async() => {
+	const validatesignup = async () => {
 		const email = document.getElementById("signup_email").value;
 		const password = document.getElementById("signup_password").value;
 
@@ -44,11 +37,10 @@ const SignUp = ({ email, password }) => {
 		if (email.includes("@")) {
 			//add code to check if user exists already
 			if (passwordRules.test(password)) {
-				const response = await handleSubmit({email, password});
-				if (response.status == 200){
+				const response = await handleSubmit(email, password);
+				if (response.status == 200) {
 					setIsValid(2);
-				}
-				else setIsValid(1);
+				} else setIsValid(1);
 				return;
 			}
 		}
@@ -68,7 +60,12 @@ const SignUp = ({ email, password }) => {
 			<button data-cy="signUpBttn" id="signup_button" onClick={validatesignup}>
 				Sign Up
 			</button>
-			{isValid == 0 && <p data-cy="signUpError">Please enter a valid email or password. Passwords need to have minimum 10 characters, uppercase, lowercase and special character.</p>}
+			{isValid == 0 && (
+				<p data-cy="signUpError">
+					Please enter a valid email or password. Passwords need to have minimum 10 characters,
+					uppercase, lowercase and special character.
+				</p>
+			)}
 			{isValid == 1 && <p data-cy="signUpError">Email already exists.</p>}
 		</>
 	);
