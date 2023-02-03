7d3851e33a7c55279693eb15b736ec25fd23ba98
Ramotalai Abdulazeez <sc19ra@leeds.ac.uk>
2023-02-03 13:00:23 +0000

    Revert "Merge branch 'add_logout_ramat' into 'master'"
    This reverts merge request !22

--- src/components/Kevin/Kevin.js
+++ src/components/Kevin/Kevin.js
@@ -2,18 +2,15 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import WebcamStreamCapture from "../Webcam";
 
-import Header from "../Header";
-
 import "../App/App.css";
 
 class Kevin extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Header />
-
-				<h1>Kevin's app</h1>
-
+				<header className="App-header">
+					<h1>Kevin's app</h1>
+				</header>
 				<div className="App-body">
 					<Link to="/home">
 						<button> Back </button>
--- src/components/Lanre/Lanre.js
+++ src/components/Lanre/Lanre.js
@@ -3,15 +3,13 @@ import { Link } from "react-router-dom";
 import WebcamStreamCapture from "../Webcam";
 import "../App/App.css";
 
-import Header from "../Header";
-
 class Lanre extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Header />
-
-				<h1>Lanre's app</h1>
+				<header className="App-header">
+					<h1>Lanre's app</h1>
+				</header>
 				<div className="App-body">
 					<Link to="/home">
 						<button> Back </button>
