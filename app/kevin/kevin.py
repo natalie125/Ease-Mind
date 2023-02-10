abee6012a5cd6252e2a38ded29828500eb1c2844
alex.cheong <alex.cheong@somin.ai>
2023-02-10 13:20:51 +0000

    split off the backend so that everyone has their invididual python modules to work on without messing with views.py

---
+++ app/kevin/kevin.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, Response, request, flash, redirect, url_for, session, jsonify
+from app import app, models, bcrypt, db
+
+#############################################################
+# ROUTE FOR ALEX'S APP
+# ^^^^^^^^^^^^^^^^^^^^^^^
+@app.route('/kevin', methods=['GET', 'POST'])
+def kevin():
+    if request.method == 'GET' or request.method == 'POST':
+        return "Kevin's App Requested"
---
+++ app/lanre/lanre.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, Response, request, flash, redirect, url_for, session, jsonify
+from app import app, models, bcrypt, db
+
+#############################################################
+# ROUTE FOR ALEX'S APP
+# ^^^^^^^^^^^^^^^^^^^^^^^
+@app.route('/lanre', methods=['GET', 'POST'])
+def lanre():
+    if request.method == 'GET' or request.method == 'POST':
+        return "Lanre's App Requested"
