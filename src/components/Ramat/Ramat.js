91daa83fd4f6333df793fe0a50711dd464b57775
ramat✨ <wonuola01@yahoo.co.uk>
2023-02-23 10:13:39 +0000

    start sending images

--- src/components/Ramat/Ramat.js
+++ src/components/Ramat/Ramat.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import axios from "axios";
 import { Link } from "react-router-dom";
-import FormData from "form-data";
+import WebcamCapture from "../camera/camera";
 
 // import WebcamStreamCapture from "../Webcam";
 
@@ -10,53 +9,13 @@ import Header from "../Header/Header";
 import "../App/App.css";
 
 class Ramat extends Component {
-	postPicture = async (image) => {
-		let BASEURL = "";
-		process.env.NODE_ENV === "development"
-			? (BASEURL = process.env.REACT_APP_DEV)
-			: (BASEURL = process.env.REACT_APP_PROD);
-
-		const testImg = 
-		let data = new FormData();
-		data.append("image", "test_face.png");
-		// e.preventDefault();
-		const response = await axios
-			.post(BASEURL + "ramat", data, {
-				headers: {
-					accept: "application/json",
-					"Accept-Language": "en-US,en;q=0.8",
-					"Content-Type": `multipart/form-data; boundary=${data._boundary}`,
-				},
-			})
-			.then((response) => {
-				console.log(response);
-				console.log(response.status);
-
-				if (response) {
-					console.log("Response received");
-				}
-				return response;
-			});
-		console.log(response);
-		return response;
-	};
-
 	render() {
 		return (
 			<div className="Ramat">
 				<Header />
 				<h1>Ramat's app</h1>
 				<div className="App-body">
-					<button
-						className="authentication-button"
-						data-cy="ramatPostButton"
-						type="submit"
-						onClick={async () => {
-							await this.postPicture();
-						}}
-					>
-						Post picture
-					</button>
+					<WebcamCapture url="ramat" />
 					<Link to="/home">
 						<button> Back </button>
 					</Link>
