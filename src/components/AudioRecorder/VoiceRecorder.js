98d6b1c11d17c22e20511b0d7e2d96af5fef2eca
ramat✨ <wonuola01@yahoo.co.uk>
2023-03-15 20:44:36 +0000

    testing recorder-js

--- src/components/AudioRecorder/VoiceRecorder.js
+++ src/components/AudioRecorder/VoiceRecorder.js
@@ -1,8 +1,7 @@
-import { AudioRecorder, useAudioRecorder } from "react-audio-voice-recorder";
+import Recorder from "recorder-js";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CantinaBand3 from "./CantinaBand3.wav";
-import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 
 let BASEURL = "";
 process.env.NODE_ENV === "development"
@@ -10,80 +9,85 @@ process.env.NODE_ENV === "development"
 	: (BASEURL = process.env.REACT_APP_PROD);
 
 const VoiceRecorder = (props) => {
-	const [voiceRecording, setVoiceRecording] = useState(null);
+	const [isRecording, setIsRecording] = useState(false);
+	const [audioBlob, setAudioBlob] = useState(null);
 
-	const recorderControls = useAudioRecorder();
-	const convertWebmToMp3 = async (blob) => {
-		const ffmpeg = createFFmpeg({ log: false });
-		await ffmpeg.load();
+	const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
-		const inputName = "input.webm";
-		const outputName = "audio.wav";
+	const recorder = new Recorder(
+		audioContext
+		// An array of 255 Numbers
+		// You can use this to visualize the audio stream
+		// If you use react, check out react-wave-stream
+		//onAnalysed: (data) => console.log(data),
+	);
 
-		ffmpeg.FS("writeFile", inputName, await fetch(blob).then((res) => res.arrayBuffer()));
+	navigator.mediaDevices
+		.getUserMedia({ audio: true })
+		.then((stream) => recorder.init(stream))
+		.catch((err) => console.log("Uh oh... unable to get stream...", err));
 
-		await ffmpeg.run("-i", inputName, outputName);
-
-		const outputData = ffmpeg.FS("readFile", outputName);
-		const outputBlob = new Blob([outputData.buffer], { type: "audio/wav" });
-
-		return outputBlob;
+	const startRecording = () => {
+		recorder
+			.start()
+			.then(() => {
+				setIsRecording(true);
+			})
+			.catch((e) => {
+				console.error(e);
+			});
 	};
 
-	const addAudioElement = async (blob) => {
+	const stopRecording = () => {
+		// buffer is an AudioBuffer
+		recorder
+			.stop()
+			.then(({ buffer, blob }) => {
+				console.log(blob);
+
+				setIsRecording(false);
+				setAudioBlob(blob);
+				console.log(buffer);
+				download();
+			})
+			.catch((e) => {
+				console.error(e);
+			});
+	};
+
+	const download = () => {
+		addAudioElement();
+		//Recorder.download(audioBlob, "my-audio-file"); // downloads a .wav file
+	};
+
+	const addAudioElement = async () => {
 		// remove existing audio component
 		if (document.body.contains(document.getElementById("recording"))) {
 			document.body.removeChild(document.getElementById("recording"));
 		}
 
-		// convert webm blob to wav blob
-		const wavBlob = await convertWebmToMp3(blob);
-
 		// create new audio component
-		const url = URL.createObjectURL(wavBlob);
+		const url = URL.createObjectURL(audioBlob);
 		const audio = document.createElement("audio");
 
 		audio.setAttribute("id", "recording");
 		audio.src = url;
 		audio.controls = true;
 
-		setVoiceRecording(wavBlob);
-		console.log("Types");
-		console.log(wavBlob.type);
-		console.log(typeof wavBlob);
-		console.log(blob.type);
-		console.log(typeof blob);
-		console.log("-------------");
 		document.body.appendChild(audio);
+	};
+
+	const doWhatever = () => {
+		setAudioBlob(null);
 	};
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 
 		let metadata = {
-			type: voiceRecording.type,
+			type: audioBlob.type,
 		};
-		let file = new File([voiceRecording], "test.wav", metadata);
-
-		// var reader = new FileReader();
-		// console.log(typeof voiceRecording);
-		// reader.onload = async function (event) {
-		// 	var fd = {};
-		// 	fd["audio"] = event.target.result;
-		// 	try {
-		// 		const response = await axios.postForm(BASEURL + props.context, {
-		// 			file: file,
-		// 			headers: {
-		// 				"Access-Control-Allow-Origin": "*",
-		// 				"Content-Type": "multipart/form-data",
-		// 			},
-		// 		});
-		// 		console.log(response);
-		// 	} catch (error) {
-		// 		console.error(error);
-		// 	}
-		// };
-		// reader.readAsDataURL(file);
+		let file = new File([audioBlob], "test.wav", metadata);
 
 		var formData = new FormData();
 		formData.append("audio", file);
@@ -102,11 +106,9 @@ const VoiceRecorder = (props) => {
 
 	return (
 		<div>
-			<AudioRecorder
-				onRecordingComplete={(blob) => addAudioElement(blob)}
-				recorderControls={recorderControls}
-			/>
-			<button onClick={recorderControls.stopRecording}>Stop recording</button>
+			<button onClick={startRecording}>Start recording</button>
+			<button onClick={stopRecording}>Stop recording</button>
+			<button onClick={doWhatever}>whatever</button>
 			<button onClick={handleSubmit}>Send recording</button>
 		</div>
 	);
