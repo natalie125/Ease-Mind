5680fd3c3679686901bc1e645fe627489025499b
Lanrayy <lanre.sodeinde1@gmail.com>
2023-03-17 14:27:20 +0000

    Made changes to fix conflick

--- app/lanre/lanre.py
+++ app/lanre/lanre.py
@@ -1,9 +1,8 @@
+from flask import Flask, render_template, Response, request, flash, redirect, url_for, session, jsonify
+from app import app, models, bcrypt, db
 import base64
 import os
 import time
-from flask import Flask, render_template, Response, request, flash, redirect, url_for, session, jsonify
-from app import app, models, bcrypt, db
-
 import cv2
 import numpy as np
 import math
