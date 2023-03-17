e13ca8010ede6f5e3f646f1f5092ab5f19b4bf63
Shreyas Honnalli <sc20sh@leeds.ac.uk>
2023-03-17 11:28:23 +0000

    integration done
    Former-commit-id: 65c25a8a7f358e4f23670ba87ad2cd9ee71687c8

--- app/shreyas/shreyas.py
+++ app/shreyas/shreyas.py
@@ -8,6 +8,7 @@ import io
 from PIL import Image
 import tensorflow as tf
 import os
+import cv2
 #############################################################
 # ROUTE FOR SHREYAS'S APP
 # ^^^^^^^^^^^^^^^^^^^^^^^
@@ -17,62 +18,99 @@ model = tf.keras.models.load_model(file_path)
 
 
 def rescale(image):
-    return image/255.0
+    return image.astype('float32')/255.0
 
 
-def resize_256_by_256(image):
-    return tf.image.resize(image, size=[256, 256])
+def resize_image(image_np, new_size):
+    resize_image = cv2.resize(image_np, new_size)
+    return resize_image
 
 
-def color_correct(image):
-    image = tf.image.adjust_contrast(image, contrast_factor=1.5)
-    image = tf.image.adjust_brightness(image, delta=0.2)
-    # image = tf.cast(image, tf.float32)/255.0
-    return image
+def convert_to_grayscale(images_np):
+    gray_np = cv2.cvtColor(images_np, cv2.COLOR_BGR2GRAY)
+    return gray_np
 
 
-def rgb_to_ycbcr(rgb_image):
-    R, G, B = tf.split(rgb_image, num_or_size_splits=3, axis=-1)
-    Y = 0.299 * R + 0.587 * G + 0.114 * B
-    Cb = -0.169 * R - 0.331 * G + 0.5 * B + 128
-    Cr = 0.5 * R - 0.419 * G - 0.081 * B + 128
-    ycbcr_image = tf.stack([Y, Cb, Cr], axis=3)
-    ycbcr_image = tf.squeeze(ycbcr_image, axis=-1)
-    return ycbcr_image
+def apply_gaussian_blur(images_np):
+    # second argument is kernel size. Larger the kernel size means wider distribution of weights
+    # third argument is sigma - larger the sigma the more spread out the blur effect. Pixels far away have greater influence on final output
+    # A larger kernel size can help to smooth out larger features in the image, while a larger sigma value can help to remove finer details and noise
+    blurred_image_np = cv2.GaussianBlur(images_np, (7, 7), 15)
+    return blurred_image_np
+
+
+def calculate_gradient_mag_using_canny(grayscale_np, lower_thresh=0, upper_thresh=0):
+    # second argument - grad mag threshold. Any edge with grad mag lower than this is discarded as weak edges
+    # third argument - grad mag threshold but any edge with grad mag higher considered as strong edges
+    grayscale_np = grayscale_np.astype(np.uint8)
+    edges_np = cv2.Canny(grayscale_np, lower_thresh, upper_thresh)
+    return edges_np
+
+
+def get_circles_from_edges(edges, min_radius=0, max_radius=100):
+    # first argument is the binary image of edges
+    # Second argument is variation of hough transform -
+    # variants include cv2.HOUGH_STANDARD, cv2.HOUGH_PROBABALISTIC, cv2.HOUGH_GRADIENT
+    # third argument is resolution of accumulator array - represents ratio between image resolution and resolution of accumulator array - determines granularity of search - smaller leads to more finer search
+    # fourth argument is the minimum distance between centers of detected circles
+    # param1 is the higher threshold for the canny edge detector
+    # fourth argument is Min and max radius of circles found
+    circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1, 20,
+                               param1=50, param2=30, minRadius=min_radius, maxRadius=max_radius)
+
+    if circles is not None:
+        circles = np.round(circles[0, :]).astype("int")
+        distances = np.sqrt(
+            (circles[:, 0] - edges.shape[1]/2)**2 + (circles[:, 1] - edges.shape[0]/2)**2)
+        sorted_circles = circles[np.argsort(distances)]
+        sorted_circles = np.expand_dims(sorted_circles, axis=0)
+        return sorted_circles
+
+    return circles
+
+
+def crop_image(image_np, circle):
+    square_size = circle[2] * 2
+    x_start = circle[0]-circle[2]
+    y_start = circle[1] - circle[2]
+    cropped = image_np[y_start: y_start +
+                       square_size, x_start:x_start+square_size]
+    return cropped
+
+
+def create_and_apply_circle_mask(image_np, circles):
+    if circles is not None:
+        circle = circles[0][0]
+        mask = np.zeros(image_np.shape[:2], dtype=np.uint8)
+        cv2.circle(mask, (circle[0], circle[1]),
+                   circle[2], (255, 255, 255), -1)
+        circle_result = cv2.bitwise_and(image_np, image_np, mask=mask)
+        result = crop_image(circle_result, circle)
+    else:
+        result = np.zeros_like(image_np)
+    return result
 
 
 def pipeline(image):
-    image = resize_256_by_256(image)
-    image = color_correct(image)
-    image = rgb_to_ycbcr(image)
+    blurred_np = apply_gaussian_blur(image)
+    gray_np = convert_to_grayscale(blurred_np)
+    grad_mag_np = calculate_gradient_mag_using_canny(gray_np, 0, 5)
+    circles = get_circles_from_edges(grad_mag_np, 70, 100)
+    cropped_image_np = create_and_apply_circle_mask(image, circles)
+    image = resize_image(cropped_image_np, (256, 256))
     image = rescale(image)
     return image
 
 
-def convert_to_rgb(np_img):
-    # Convert the image to 3 channels (if it is not already)
-    if np_img.ndim == 2:
-        np_img = np.tile(np_img[:, :, np.newaxis], [1, 1, 3])
-
-    # Convert the image to RGB (if it is not already)
-    if np_img.shape[2] > 3:
-        np_img = np_img[:, :, :3]
-
+def convert_to_rgb_np(img):
+    img = img.resize((256, 256))
+    np_img = np.array(img)
     return np_img
 
 
-def convert_to_rgb_np(img):
-    np_img = np.array(img)
-    np_rgb_img = convert_to_rgb(np_img)
-    return np_rgb_img
-
-
 def make_tensorflow_compatible(np_img):
-    return np.expand_dims(np_img.astype(np.float32), axis=0)
-
-
-def make_prediction(img):
-    return model.predict(img)
+    np_img = np.expand_dims(np_img.astype(np.float32), axis=0)
+    return np_img
 
 
 @app.route('/shreyas', methods=['GET', 'POST'])
@@ -89,10 +127,10 @@ def shreyas():
         img = Image.open(io.BytesIO(image_decoded))
         # Convert the PIL image object to a NumPy array with RGB channels
         np_rgb_img = convert_to_rgb_np(img)
+        np_processed_img = pipeline(np_rgb_img)
         # Convert the image to a format compatible with TensorFlow models
-        tf_img = make_tensorflow_compatible(np_rgb_img)
-        tf_img_preprocessed = pipeline(tf_img)
-        prediction = make_prediction(tf_img_preprocessed)
+        tf_img = make_tensorflow_compatible(np_processed_img)
+        prediction = model.predict(tf_img)
         print(prediction[0][0])
         if prediction[0][0] >= 0.5:
             msg = 1
