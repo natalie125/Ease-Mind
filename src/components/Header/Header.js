44c4f21b4eae39b0b16565d6ba8d0a243e1f296b
Lanrayy <lanre.sodeinde1@gmail.com>
2023-03-21 00:39:48 +0000

    Added logo link to app

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
 	const navigate = useNavigate();
@@ -11,7 +12,10 @@ const Header = () => {
 
 	return (
 		<header className="App-header-primary">
-				<h1 id="header_name"> LARKS APP</h1>
+				<Link id="header_name" to="/">
+					<h1> LARKS APP</h1>
+				</Link>
+
 				<div id="header_buttons">
 					<button data-cy="logoutBttn" id="logout_button" className="logout-button" onClick={logout}>
 						Logout
