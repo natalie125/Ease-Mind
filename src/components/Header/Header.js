456661fb49220880791335f8d7395ab306086cc0
Lanrayy <lanre.sodeinde1@gmail.com>
2023-03-24 15:22:28 +0000

    Updated header to prevent commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,28 @@
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
 	const navigate = useNavigate();
 
 	const logout = () => {
-		sessionStorage.clear();	// clear the session, including the token that keeps us logged in
+		sessionStorage.clear(); // clear the session, including the token that keeps us logged in
 		navigate("/", { replace: true }); // send us to the "/" URL
-		window.location.reload(false);	// refresh the page
+		window.location.reload(false); // refresh the page
 	};
 
 	return (
 		<header className="App-header-primary">
-				<Link id="header_name" to="/">
-					<h1> LARKS APP</h1>
-				</Link>
-
-				<div id="header_buttons">
-					<button data-cy="logoutBttn" id="logout_button" className="logout-button" onClick={logout}>
-						Logout
-					</button>
-				</div>
+			<h1 id="header_name"> LARKS APP</h1>
+			<div id="header_buttons">
+				{!props.hideHome && (
+					<Link style={{ width: "100%" }} to="/home">
+						<button className="header-button"> Home </button>
+					</Link>
+				)}
+				<button data-cy="logoutBttn" id="logout_button" className="header-button" onClick={logout}>
+					Logout
+				</button>
+			</div>
 		</header>
 	);
 };
