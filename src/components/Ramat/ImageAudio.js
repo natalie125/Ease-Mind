9133383b3e9be8f9f13a0c270d068c01616bd52a
ramat✨ <wonuola01@yahoo.co.uk>
2023-04-18 19:53:50 +0100

    init v2

--- src/components/Ramat/ImageAudio.js
+++ src/components/Ramat/ImageAudio.js
@@ -18,6 +18,10 @@ const ImageAudio = (props) => {
 	const [showAudio, setShowAudio] = useState(false);
 	const [audio, setAudio] = useState(false);
 	const [serverResponse, setServerResponse] = React.useState(null);
+	const [displayMessage, setDisplayMessage] = React.useState(null);
+	let [hasMounted, setHasMounted] = useState(false);
+
+
 
 	const submitAll = async (blob) => {
 		setAudio(blob);
@@ -50,21 +54,50 @@ const ImageAudio = (props) => {
 		setAudio(null);
 	};
 
+	const getDisplayMessage = () => {
+		const jsonResponse = JSON.parse(serverResponse)
+		const droop_prediction = jsonResponse.face_prediction
+		const dys_prediction = jsonResponse.voice_prediction
+			console.log(jsonResponse[1])
+
+		if(droop_prediction === "no droop" && dys_prediction < 75){
+			setDisplayMessage("You are not displaying any signs of facial droop or slurring.")
+		}else if(droop_prediction === "droop" && dys_prediction < 75){
+			setDisplayMessage("Uh oh, you seem to be displaying a symptom of stroke with a certainty of 56%. Call 999 immediately!")
+		}else if(droop_prediction === "no_droop" && dys_prediction > 75){
+			setDisplayMessage("Uh oh, you seem to be displaying a symptom of stroke with a certainty of 90%. Call 999 immediately!")
+		}else if(droop_prediction === "droop" && dys_prediction > 75){
+			setDisplayMessage("Uh oh, you seem to be displaying two symptom of stroke with a certainty of 50%. Call 999 immediately!")
+		}
+	}
+
+	useEffect(() => {
+		if (hasMounted && serverResponse !== null) {
+			getDisplayMessage()
+		} else {
+			setHasMounted(true);
+		}
+	}, [serverResponse]);
+
+
 	return (
 		<div className="paralysis-container">
-			<h2 style={{ marginBottom: "1%" }}>Welcome to Paralysis Analysis</h2>
 
 			{!showAudio && (
+				<>
+				<h2 style={{ marginBottom: "1%" }}>Welcome to Paralysis Analysis</h2>
+
 				<div className="paralysis-split-container">
 					<p className="paralysis-text-container">
 						Straighten your head as best as you can, keep a neutral expresssion and take a picture.
 					</p>
 					<Camera endpoint="ramat/image" returnImage={setImage} returnFinished={setShowAudio} />
 				</div>
+				</>
 			)}
 			{showAudio && !audio && (
 				<>
-					<p>Record yourself saying the following prompt and submit when ready:</p>
+					<p style={{ paddingTop: "3%", }}>Record yourself saying the following prompt and submit when ready:</p>
 					<p>
 						<em>The quick brown fox jumps over the lazy dog</em>
 					</p>
@@ -73,8 +106,8 @@ const ImageAudio = (props) => {
 			)}
 			{serverResponse && (
 				<>
-					<p>Response: {serverResponse}</p>
-					<button onClick={restart}>Restart</button>
+					<h2 style={{ paddingTop: "3%", paddingBottom: "3%" }}>{displayMessage}</h2>
+					<button className ="paralysis-button" onClick={restart}>Restart</button>
 				</>
 			)}
 		</div>
