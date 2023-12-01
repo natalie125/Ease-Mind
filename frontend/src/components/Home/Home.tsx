d1d50fb6efd1c456b3befb00368d1adea8910fdb
sc21ar <sc21ar@leeds.ac.uk>
2023-12-01 03:49:05 +0000

    Home page redesign

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Home.scss";
 
@@ -11,88 +11,107 @@ import skinscan_logo from "./logos/skinscan.png";
 import ease_mind_logo from "./logos/ease-mind.png";
 import autism_detector_logo from "./logos/autism-detector.png";
 import chatbot_logo from "./logos/chatbot.png"
+import homeImage from "../../images/home.png";
 
 interface IAppLink {
   to: string;
-  logo: any;
+  logo: string;
   alt: string;
-  dataCy: string;
+  slogan: string;
 }
 
-const AppLink = ({ to, logo, alt, dataCy }: IAppLink) => (
-  <Link className="app-link" to={to}>
-    <img data-cy={dataCy} src={logo} alt={alt} />
-  </Link>
-);
+const AppLink = ({ to, logo, alt, slogan }: IAppLink) => {
+  const [showSlogan, setShowSlogan] = useState(false);
 
-const Home = () => (
-  <div className="home">
-    <h1>Home</h1>
-    <div data-cy="homeBtnContainer">
-      <AppLink
-        to="/canopy"
-        logo={canopy_logo}
-        alt="Canopy_App_Alex"
-        dataCy="alexAppLogo"
-      />
+  return (
+    <Link
+      className="app-link"
+      to={to}
+      onMouseEnter={() => setShowSlogan(true)}
+      onMouseLeave={() => setShowSlogan(false)}
+    >
+      <div className="logo-container">
+        <img src={logo} alt={alt} />
+        {showSlogan && <div className="slogan-container">{slogan}</div>}
+      </div>
+    </Link>
+  );
+};
 
-      <AppLink
-        to="/skin-scan"
-        logo={skinscan_logo}
-        alt="Skin-Scan_App_Kevin"
-        dataCy="kevinAppLogo"
-      />
+const Home = () => {
+  const appLinks: IAppLink[] = [
+    {
+      to: "/canopy",
+      logo: canopy_logo,
+      alt: "Canopy_App_Alex",
+      slogan: "Empowering healthcare solutions."
+    },
+    {
+      to: "/skin-scan",
+      logo: skinscan_logo,
+      alt: "Skin-Scan_App_Kevin",
+      slogan: "Empowering healthcare solutions."
+    },
+    {
+      to: "/dipstik",
+      logo: dipstik_logo,
+      alt: "Dipstik_App_Lanre",
+      slogan: "Empowering healthcare solutions."
+    },
+    {
+      to: "/paralysis-analysis",
+      logo: paralysis_analysis_logo,
+      alt: "Stroke_App_Ramat",
+      slogan: "Empowering healthcare solutions."
+    },
+    {
+      to: "/shreyas/tonsillitis_instructions",
+      logo: tonsillitis_detector_logo,
+      alt: "Tonsilitis_App_Shreyas",
+      slogan: "Empowering healthcare solutions."
+    },
+    {
+      to: "/roots-radar",
+      logo: roots_radar_logo,
+      alt: "Roots Radar App",
+      slogan: "Empowering healthcare solutions."
+    },
+    {
+      to: "/EaseMind",
+      logo: ease_mind_logo,
+      alt: "EaseMind App",
+      slogan: "Empowering healthcare solutions."
+    },
+    {
+      to: "/autism_instructions",
+      logo: autism_detector_logo,
+      alt: "Autism_App",
+      slogan: "Always Unique Totally Intelligent Sometimes Mysterious"
+    },
+    {
+      to: "/food_allergy_chatbot",
+      logo: chatbot_logo,
+      alt: "chatbot_Logo",
+      slogan: "Empowering healthcare solutions."
+    },
+  ];
 
-      <AppLink
-        to="/dipstik"
-        logo={dipstik_logo}
-        alt="Dipstik_App_Lanre"
-        dataCy="lanreAppLogo"
-      />
-
-      <AppLink
-        to="/paralysis-analysis"
-        logo={paralysis_analysis_logo}
-        alt="Stroke_App_Ramat"
-        dataCy="paralysisAnalysisLink"
-      />
-
-      <AppLink
-        to="/shreyas/tonsillitis_instructions"
-        logo={tonsillitis_detector_logo}
-        alt="Tonsilitis_App_Shreyas"
-        dataCy="shreyasAppLogo"
-      />
-
-      <AppLink
-        to="/roots-radar"
-        logo={roots_radar_logo}
-        alt="Roots Radar App"
-        dataCy="Roots-Radar-App"
-      />
-
-      <AppLink
-        to="/EaseMind"
-        logo={ease_mind_logo}
-        alt="EaseMind App"
-        dataCy="easelogo"
-      />
-
-      <AppLink
-        to="/autism_instructions"
-        logo={autism_detector_logo}
-        alt="Autism_App"
-        dataCy="ayeshasAppLogo"
-      />
-
-      <AppLink
-        to="/food_allergy_chatbot"
-        logo={chatbot_logo}
-        alt="chatbot_Logo"
-        dataCy="chatbotlogo"
-      />
+  return (
+    <div className="home">
+      <div className="app-links-container">
+        {appLinks.map((app, index) => (
+          <AppLink
+            key={index}
+            to={app.to}
+            logo={app.logo}
+            alt={app.alt}
+            slogan={app.slogan}
+          />
+        ))}
+      </div>
+      <img src={homeImage} alt="home" className="home-image" />
     </div>
-  </div>
-);
+  );
+};
 
 export default Home;
