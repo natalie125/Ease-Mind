45b98130981bedf6de2d3171eed989ef0eed4e5e
Archie Adams <archiea2002@gmail.com>
2024-01-04 13:13:28 +0000

    fix, refactor and extend signin and signup backend tests

--- backend/tests/unit_tests/signup_route.py
+++ backend/tests/unit_tests/signup_route.py
@@ -1,96 +1,42 @@
 import pytest
-from app import app, db, models
 import json
 import time
+from app import app
 
-
-#############################################################
-# BEFORE RUNNING PYTESTS YOU HAVE TO MAKE SURE TESTING VARIABLE IS SET TO TRUE, OTHERWISE WE WILL WRITE TO app.db
-# ^^^^^^^^^^^^^^^^^^^^^^^
-
-@pytest.fixture()
-def client():
-    """
-    Configures the app for testing
-    :return: App for testing
-    """
-    client = app.test_client()
-    yield client
-
+def post_register(email, password):
+    return app.test_client().post(
+        '/register',
+        data=json.dumps({
+            'email': email,
+            'password': password
+        }),
+        headers={
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    )
 
 @pytest.mark.unit
-def test_register_new_client(client):
-    """
-    GIVEN: A POST request to the /register endpoint with login credentials
-    WHEN: A user registers a new account
-    THEN: A JSON with a welcome message should be generated
-
-    Tests that we can make a POST request to the api with details and stores it in database
-    """
-
-    with client:
-        mimetype = 'application/json'
-
-        headers = {
-            'Content-Type': mimetype,
-            'Accept': mimetype
-        }
-
-        timestamp = str(int(time.time()))
-        data = {
-            'email': timestamp+'@gmail.com',
-            'password': 'admin'
-        }
-        response = client.post(
-            '/register', data=json.dumps(data), headers=headers)
-        with app.app_context():
-            assert response.status_code == 200
-
-        models.User_Login_Test.query.filter_by(
-            email=timestamp+'@gmail.com').delete()
-        db.session.commit()
-
+def test_post_register_new_user():
+    response = post_register(str(int(time.time())) + '@gmail.com', 'admin')
+    assert response.status_code == 200
+    assert response.json["msg"] == "New user added."
+    assert response.json["token"] != None
 
 @pytest.mark.unit
-def test_register_existing_client(client):
-    """
-    GIVEN: A POST request to the /register endpoint with existing login credentials
-    WHEN: A user registers with an existing account
-    THEN: A JSON with an error code should be returned indicating failure
-
-    Tests that we can make a POST request to the api with details and it doesnt store in db
-    """
-    with client:
-        mimetype = 'application/json'
-        headers = {
-            'Content-Type': mimetype,
-            'Accept': mimetype
-        }
-        data = {
-            'email': 'admin@gmail.com',
-            'password': 'admin'
-        }
-        response = client.post(
-            '/register', data=json.dumps(data), headers=headers)
-        with app.app_context():
-            assert response.status_code == 401
-            assert response.json['msg'] == "Username taken"
-
+def test_post_register_existing_user():
+    response = post_register('admin@gmail.com', 'admin')
+    assert response.status_code == 409
+    assert response.json['msg'] == "Username taken."
 
 @pytest.mark.unit
-def test_register_missing_details(client):
-    with client:
-        mimetype = 'application/json'
-        headers = {
-            'Content-Type': mimetype,
-            'Accept': mimetype
-        }
-        data = {
-            'email': '',
-            'password': ''
-        }
-        response = client.post(
-            '/register', data=json.dumps(data), headers=headers)
-        with app.app_context():
-            assert response.status_code == 401
-            assert response.json['msg'] == "One or more credentials not provided"
+def test_post_register_invalid_email():
+    response = post_register('', 'admin')
+    assert response.status_code == 400
+    assert response.json['msg'] == "No email was provided."
+
+@pytest.mark.unit
+def test_post_register_invalid_password():
+    response = post_register('admin@gmail.com', '')
+    assert response.status_code == 400
+    assert response.json['msg'] == "No password was provided."
