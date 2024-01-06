dff6ec5ea71096dd5feac28c546497e2d473deec
Jeffery <fy19cwt@leeds.ac.uk>
2024-01-06 21:33:45 +0000

    comment

--- frontend/src/components/Camera/Camera.test.tsx
+++ frontend/src/components/Camera/Camera.test.tsx
@@ -3,6 +3,12 @@ import * as renderer from 'react-test-renderer';
 import Webcam from 'react-webcam';
 
 //webcam render test from https://github.com/mozmorris/react-webcam/blob/master/src/__tests__/react-webcam.test.tsx
+
+//The test uses React's react-test-renderer to perform a snapshot test on a Webcam component from the react-webcam library.
+//uses the renderer.create() method from react-test-renderer to render the Webcam component. Creates a test renderer instance, which can be used to generate a snapshot.
+//Snapshot Testing: After rendering the component, the test uses expect(tree.toJSON()).toMatchSnapshot(); to generate a snapshot of the Webcam component's rendered output.
+//If the current snapshot matches the stored snapshot, the test will pass, indicating that the rendered output of the component has not changed unexpectedly
+
 it('renders correctly', () => {
   const tree = renderer
     .create(
@@ -33,6 +39,8 @@ it('renders correctly', () => {
 
   expect(tree.toJSON()).toMatchSnapshot();
 });
+
+//commenting the part for video testing
 /*
 it('sets <video/> muted to false when props.audio is true', () => {
   const tree = renderer
