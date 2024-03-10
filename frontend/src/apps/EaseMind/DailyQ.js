89266d5f118c489b133e8d778e8c87ddb02dbbfa
sc21nl <sc21nl@leeds.ac.uk>
2024-03-10 16:08:12 +0000

    add styling to daily question

--- frontend/src/apps/EaseMind/DailyQ.js
+++ frontend/src/apps/EaseMind/DailyQ.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
-import './AnxietyLevelTest.css';
+import './DailyQ.css';
 import { AuthTokenContext } from '../../App';
 
 function DailyQuestions() {
