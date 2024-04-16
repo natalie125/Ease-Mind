0721f43503728f6ae5ffe513554c0f27c6c86c7d
sc21nl <sc21nl@leeds.ac.uk>
2024-04-17 00:11:14 +0100

    home page update

--- frontend/src/apps/EaseMind/EaseMind.js
+++ frontend/src/apps/EaseMind/EaseMind.js
@@ -5,7 +5,7 @@ import ePersonalDetailsImage from './images/e_personal_details.png';
 import anxietyTestImage from './images/anxiety_test.png';
 import reportImage from './images/report.png';
 import dailyQuestionnaireImage from './images/daily_q.png';
-import chatBoxImage from './images/chatbox.png';
+import chatBoxImage from './images/chat_box.png';
 import PopUp from './PopUp';
 import ChatBox from './ChatBox';
 
@@ -28,6 +28,7 @@ function EaseMind() {
 
   return (
     <div className="easeMindFullPage">
+      <h1 className="easeMindHeader">Feeling anxious? We are here to help!</h1>
       <div className="easeMindFeatures">
         <Link to="/EaseMind_personal_details" className="easeMindFeatureItem">
           <img src={ePersonalDetailsImage} alt="Personal Details" />
@@ -45,10 +46,13 @@ function EaseMind() {
           <img src={dailyQuestionnaireImage} alt="Daily Questionnaire" />
           <span>Complete Daily Questionnaire</span>
         </Link>
-        <button type="button" className="easeMindFeatureItem" onClick={toggleChatBox}>
-          <img src={chatBoxImage} alt="Chat Box" />
-          <span>{showChatBox ? 'Hide Chat' : 'Chat Box'}</span>
-        </button>
+        {!showChatBox && (
+          <button type="button" className="easeMindchatboxbutton" onClick={toggleChatBox}>
+            <img src={chatBoxImage} alt="Chat Box" />
+            <span>Chat with us!</span>
+            {/* {showChatBox ? 'Hide Chat' : 'Chat Box'} */}
+          </button>
+        )}
       </div>
       {showChatBox && <ChatBox />}
       {showPopUp && <PopUp onClose={handlePopUpClose} />}
@@ -57,3 +61,4 @@ function EaseMind() {
 }
 
 export default EaseMind;
+// https://www.freepik.com/icon/chat-bubble_8224141#fromView=keyword&page=1&position=4&uuid=465504c4-7b9f-4996-8e1a-ebc323fad9fa
--- frontend/src/apps/EaseMind/PersonalDetails.js
+++ frontend/src/apps/EaseMind/PersonalDetails.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './EaseMind.css';
+import './AnxietyLevelTest.css';
 import { AuthTokenContext } from '../../App';
 
 function PersonalDetails() {
