7defbfe0de696dd939d9f75c1675b111eb2fd683
marilenaManoli <sc21m2m@leeds.ac.uk>
2024-05-03 13:48:18 +0100

    depressiless.py

--- backend/app/depressiLess/DepressiLess.py
+++ backend/app/depressiLess/DepressiLess.py
@@ -23,7 +23,6 @@ gpt_model.eval()
 app = Flask(__name__)
 CORS(app)
 
-"""
 google_drive_model_id = '10BdhMe8kXoxw5tPWLdv8yIYwTnYGuNZz'
 model_directory = 'app/depressiLess/models'
 model_filename = 'my_depression_model.zip'
@@ -55,7 +54,6 @@ def initialize_model():
     my_depression_model = AutoModelForSequenceClassification.from_pretrained(model_path)
     tokenizerModel = AutoTokenizer.from_pretrained(model_path)
     my_depression_model.eval()
-"""
 
 @app.errorhandler(Exception)
 def handle_unexpected_error(error):
